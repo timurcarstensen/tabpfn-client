1f4d7edc2f99358077d4497590f3abad28090b70
Timur M. Carstensen <timurcarstensen@gmail.com>
2024-09-03 16:10:37 +0200

    also logging shapes

--- tabpfn_client/client.py
+++ tabpfn_client/client.py
@@ -177,9 +177,13 @@ class ServiceClient:
             result = {"probas": result}
 
         print("keys in result", result.keys())
-        
+
         for k in result:
             result[k] = np.array(result[k])
+        
+        for k, v in result.items():
+            if isinstance(v, np.ndarray) or isinstance(v, torch.Tensor):
+                print(k, v.shape)
 
         return result
 
